592cd4300a8a5a246172e68c84cf0c6d64525584
Renmy <renmyenriquez@gmail.com>
2024-05-23 21:24:47 -0400

    before-merge

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,6 +6,12 @@ const AboutPage = () => {
       <h1 className="p-5 text-2xl border-b-4 border-b-slate-500 pb-7 font-semibold text-slate-700 ">
         About
       </h1>
+      <div className="flex p-4">
+        <div className="w-2/3 flex flex-col gap-5">
+          <img src="" alt="" />
+        </div>
+        <div className="w-1/3 flex flex-col gap-5"></div>
+      </div>
     </div>
   );
 };
